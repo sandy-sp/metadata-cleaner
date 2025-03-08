b71f29de4d3b7f07e4f3c2f62fe449856306a6ab
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-08 06:48:32 -0500

    Update __init__.py

--- metadata_cleaner/config/__init__.py
+++ metadata_cleaner/config/__init__.py
@@ -1,4 +1,6 @@
 # This file is used to import all the settings from the settings.py file
 
 from .settings import *
+
 __all__ = ['DEFAULT_OUTPUT_FOLDER', 'ENABLE_PARALLEL_PROCESSING', 'LOG_LEVEL', 'SUPPORTED_FORMATS']
+
