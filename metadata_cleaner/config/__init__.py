7a066705755636b49a8a58fa8a9bb7c5ffab06e9
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-08 01:06:23 -0500

    Fixing Empty __init__.py Files

--- metadata_cleaner/config/__init__.py
+++ metadata_cleaner/config/__init__.py
@@ -1,4 +1,4 @@
-# metadata_cleaner/config/__init__.py
-from .settings import *
+# This file is used to import all the settings from the settings.py file
 
+from .settings import *
 __all__ = ['DEFAULT_OUTPUT_FOLDER', 'ENABLE_PARALLEL_PROCESSING', 'LOG_LEVEL', 'SUPPORTED_FORMATS']
